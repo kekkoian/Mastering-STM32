24c8a7528eb0e27e1cfa8df616b468b53dbd0fa8
kekkoian <40660100+kekkoian@users.noreply.github.com>
2021-03-24 12:51:03 +0100

    ch11 ex 3

--- TIM_ExternalClockMode2/Core/Src/stm32f4xx_hal_msp.c
+++ TIM_ExternalClockMode2/Core/Src/stm32f4xx_hal_msp.c
@@ -105,6 +105,9 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
     GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
+    /* TIM3 interrupt Init */
+    HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(TIM3_IRQn);
   /* USER CODE BEGIN TIM3_MspInit 1 */
 
   /* USER CODE END TIM3_MspInit 1 */
@@ -133,6 +136,8 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
     */
     HAL_GPIO_DeInit(GPIOD, GPIO_PIN_2);
 
+    /* TIM3 interrupt DeInit */
+    HAL_NVIC_DisableIRQ(TIM3_IRQn);
   /* USER CODE BEGIN TIM3_MspDeInit 1 */
 
   /* USER CODE END TIM3_MspDeInit 1 */
